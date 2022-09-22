e7dce43c82dd2c9dfc4bcc333fcd936feed31899
ehddnr177 <ehddnr177@naver.com>
2022-09-22 14:17:25 +0900

    # 변경사항 1. favicon 적용

--- src/main/java/dev/kkukkie_bookstore/controller/file/image/ImageController.java
+++ src/main/java/dev/kkukkie_bookstore/controller/file/image/ImageController.java
@@ -19,7 +19,6 @@ import java.util.Objects;
 
 @Slf4j
 @RestController
-@RequestMapping("**/images")
 public class ImageController {
 
     private final Environment environment;
@@ -29,7 +28,7 @@ public class ImageController {
     }
 
     // http://127.0.0.1:8080/images/20cb9808-e248-406a-871b-01a0f63b4d9b.jpeg
-    @GetMapping(value = "{name}", produces = MediaType.IMAGE_JPEG_VALUE)
+    @GetMapping(value = "**/images/{name}", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<byte[]> getProfileImage(@PathVariable("name") String name) throws IOException {
         String filePath = FileManager.concatFilePath(environment.getProperty("spring.servlet.multipart.location"), name);
         InputStream imageStream = new FileInputStream(
